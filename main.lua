3526f2c7ce60ef4e4ba0c23874645ff6b24981a6
Raúl Contreras <raulc98@gmail.com>
2019-06-23 18:05:03 +0200

    Added collision detection, mousepressed and score

--- main.lua
+++ main.lua
@@ -1,4 +1,14 @@
 function love.load()
+  button = {}
+  button.x = 200
+  button.y = 200
+  button.size = 50
+  button.isInside = function (x, y) return math.sqrt((button.y - y)^2 + (button.x - x)^2) <= button.size end
+
+  score = 0
+  timer = 0
+
+  myFont = love.graphics.newFont(40)
 
 end
 
@@ -7,8 +17,16 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.setColor(0, 0, 255/255, 1)
-  love.graphics.rectangle('fill', 200, 400, 200, 100)
   love.graphics.setColor(255/255, 0, 0, 1)
-  love.graphics.circle('fill', 100, 100, 100)
+  love.graphics.circle('fill', button.x, button.y, button.size)
+
+  love.graphics.setFont(myFont)
+  love.graphics.setColor(255/255, 255/255, 255/255, 1)
+  love.graphics.print('Score:' .. score)
+end
+
+function love.mousepressed(x, y, b)
+  if b == 1 and button.isInside(x, y) then
+      score = score + 1
+  end
 end
